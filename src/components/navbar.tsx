8f8ada8a994c40e01d3a6681ca4edfe5a79429ac
Rafal Szostak <rafal.szostak2@gmail.com>
2024-03-26 19:50:14 +0100

    feat: Add navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <div className="h-16 mx-auto  max-w-4xl  flex items-center justify-center text-black border-b text-lg font-semibold">
+      Mediporta - zadanie rekrutacyjne
+    </div>
+  )
+}
